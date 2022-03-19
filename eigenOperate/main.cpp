e8b89a1c38d62e8ac4ceaa8ca9392802250ed842
chenfei-nreal <fchen@nreal.ai>
2022-03-19 15:35:23 +0800

    Eigen base operate

--- eigenOperate/main.cpp
+++ eigenOperate/main.cpp
@@ -6,9 +6,7 @@ int main(int argc, char **argv) {
   matrix4f << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16;
 
   /** 1. 块操作 **/
-
   // 1.1 block用于左值操作
-
   // 固定大小子矩阵
   std::cout << "Block in the middle" << std::endl;
   std::cout << matrix4f.block<2, 2>(1, 1) << std::endl;
@@ -17,7 +15,6 @@ int main(int argc, char **argv) {
     std::cout << "Block of size " << i << std::endl;
     std::cout << matrix4f.block(0, 0, i, i) << std::endl << std::endl;
   }
-
   // 1.2 block用于右值操作
   Eigen::Array44f m = Eigen::Array44f::Constant(0.6);
   matrix4f.block<2, 2>(1, 1) = m.block(1, 1, 2, 2);
@@ -25,7 +22,6 @@ int main(int argc, char **argv) {
   std::cout << matrix4f << std::endl << std::endl;
 
   /** 2. 行子式和列子式 **/
-
   std::cout << "2nd Row: " << matrix4f.row(1) << std::endl << std::endl;
   m.col(2) += 3 * m.col(3);
   std::cout << "After adding 3 times the first colunm into third colum, the "
@@ -33,7 +29,6 @@ int main(int argc, char **argv) {
   std::cout << matrix4f << std::endl << std::endl;
 
   /** 3. 边角子矩阵 **/
-
   std::cout << "leftCols(2) = " << std::endl << matrix4f.leftCols(2) << std::endl << std::endl;
   std::cout << "bottomRows<2>() = " << std::endl
             << matrix4f.bottomRows<2>() << std::endl
